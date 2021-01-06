2a2789ed0a389df1d02d710fb92669a1ff4b0ec0
Alex Clements <71945393+clements1992@users.noreply.github.com>
2021-01-06 16:37:45 +0000

    added food

--- src/Beers.java
+++ src/Beers.java
@@ -1,36 +1,32 @@
 
-public enum Beers
-{
-    NONE("none",0),
-    REDSTRIPE("redstripe",5),
-    NEWCASTLE("newcastle",6),
-    CAMDEN_HELLS("camdenhells",7),
-    STRONGBOW("strongbow",4),
-    HOPHOUSE("hophouse",6),
-    VODKA("vodka",35),
-    TEQUILA("tequila",50),
-    KEBAB("kebab", -2),
-    CHICKEN("chicken", -8),
-    NUTS("nuts", -4);
+public enum Beers {
+    CAMDEN_HELLS("camdenhells", 7),
+    CHICKEN("chicken", 6),
+    HOPHOUSE("hophouse", 6),
+    KEBAB("kebab", -8),
+    NEWCASTLE("newcastle", 6),
+    NONE("none", 0),
+    NUTS("nuts", -2),
+    REDSTRIPE("redstripe", 5),
+    STRONGBOW("strongbow", 4),
+    TEQUILA("tequila", 50),
+    VODKA("vodka", 35);
 
 
     private static int alcoholLevel;
     private String beerString;
 
 
-    Beers(String beerString,int alcoholLevel)
-    {
+    Beers(String beerString, int alcoholLevel) {
         this.beerString = beerString;
     }
 
 
-    public String toString()
-    {
+    public String toString() {
         return beerString;
     }
 
-    public static int getAlcoholLevel()
-    {
+    public static int getAlcoholLevel() {
         return alcoholLevel;
     }
 
