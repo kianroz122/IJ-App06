80f57c97ea18ead598e0806251168de03cce9f66
Alex Clements <71945393+clements1992@users.noreply.github.com>
2021-01-07 12:22:33 +0000

    -

---
+++ src/Beers.java
@@ -0,0 +1,31 @@
+
+public enum Beers
+{
+    NONE("none",0),
+    REDSTRIPE("redstripe",5),
+    NEWCASTLE("newcastle",6),
+    CAMDEN_HELLS("comdenhills",7),
+    STRONGBOW("strongbow",4),
+    HOPHOUSE("hophouse",6),
+    VODKA("vodka",35),
+    TEQUILA("tequila",50);
+
+    private String beerString;
+    private int alcoholLevel;
+
+    Beers(String beerString,int alcoholLevel)
+    {
+        this.beerString = beerString;
+    }
+
+    public String toString()
+    {
+        return beerString;
+    }
+
+    public int getAlcoholLevel()
+    {
+        return alcoholLevel;
+    }
+
+}
