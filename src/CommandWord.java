410da54e0b80cec1e9225231d9e074693880c2eb
Alex Clements <71945393+clements1992@users.noreply.github.com>
2021-01-07 12:22:37 +0000

    -

--- src/CommandWord.java
+++ src/CommandWord.java
@@ -10,7 +10,9 @@ public enum CommandWord
     // A value for each command word along with its
     // corresponding user interface string.
     GO("go"), QUIT("quit"),
-    HELP("help"), UNKNOWN("?");
+    HELP("help"), UNKNOWN("?"),
+    PICKUP("pickup"), DROP("drop"),
+    DRINK("drink");
     
     // The command string.
     private String commandString;
